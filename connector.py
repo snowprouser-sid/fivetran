5b08a9d7cab9fe3234e6d0ae626a5e4d0cdc3538
snowprouser-sid <snowprouser@gmail.com>
2025-09-28 18:05:02 +0530

    Deploy largeset

--- connector.py
+++ connector.py
@@ -1,7 +1,5 @@
-# This example demonstrates how you can write a complex connector comprising multiple .py files.
-# This includes the __init__.py which is needed to ensure the timestamp_serializer module is correctly recognized.
-# The timestamp_serializer module shown in this example is used to handle scenarios where the source sends timestamps in two different formats.
-# It assumes that the source will send timestamps in UTC timezone.
+# This is a simple example for how to work with the fivetran_connector_sdk module.
+# This example demonstrates how to work with API which has large data set in the response.
 # See the Technical Reference documentation (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
 # and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details
 
@@ -15,28 +13,14 @@ from fivetran_connector_sdk import Logging as log
 # For supporting Data operations like Upsert(), Update(), Delete() and checkpoint()
 from fivetran_connector_sdk import Operations as op
 
-# Import self written modules
-from timestamp_serializer import TimestampSerializer
+import pandas as pd
 
+# Import the requests module for making HTTP requests, aliased as rq.
+import requests as rq
 
-def schema(configuration: dict):
-    """
-    Define the schema function which lets you configure the schema your connector delivers.
-    See the technical reference documentation for more details on the schema function:
-    https://fivetran.com/docs/connectors/connector-sdk/technical-reference#schema
-    Args:
-        configuration: a dictionary that holds the configuration settings for the connector.
-    """
-    return [
-        {
-            "table": "event",  # Name of the table in the destination.
-            "primary_key": ["name"],  # Primary key column(s) for the table.
-            "columns": {  # Define the columns and their data types.
-                "name": "STRING",  # String column for the name.
-                "timestamp": "UTC_DATETIME",  # UTC date-time column for the timestamp
-            },
-        }
-    ]
+# Define the constant values
+PAGE_LIMIT = 100
+BASE_URL = "https://pokeapi.co/api/v2/pokemon"
 
 
 def update(configuration: dict, state: dict):
@@ -49,41 +33,52 @@ def update(configuration: dict, state: dict):
         state: A dictionary containing state information from previous runs
         The state dictionary is empty for the first sync or for any full re-sync
     """
-    log.warning("Example: QuickStart Examples - Simple Three Step Cursor")
+    log.warning("Example: QuickStart Examples - Large Data Set With Pagination")
 
-    timestamp_serializer = TimestampSerializer()
-
-    row_1 = {"name": "Event1", "timestamp": "2024/09/24 14:30:45"}
-    row_1["timestamp"] = timestamp_serializer.serialize(row_1["timestamp"])
-    op.upsert(table="event", data=row_1)
-
-    row_2 = {"name": "Event2", "timestamp": "2024-09-24 10:30:45"}
-    row_2["timestamp"] = timestamp_serializer.serialize(row_2["timestamp"])
-    op.upsert(table="event", data=row_2)
-
-    # Save the progress by checkpointing the state. This is important for ensuring that the sync process can resume
-    # from the correct position in case of next sync or interruptions.
-    # Learn more about how and where to checkpoint by reading our best practices documentation
-    # (https://fivetran.com/docs/connectors/connector-sdk/best-practices#largedatasetrecommendation).
-    op.checkpoint(state)
+    offset = 0
+    api_endpoint = BASE_URL + "?offset=" + str(offset) + "&limit=" + str(PAGE_LIMIT)
+    while True:
+        next_url, pokemons_df, next_offset = get_data(api_endpoint, offset)
+        for index, row in pokemons_df.iterrows():
+            op.upsert(table="pokemons", data={col: row[col] for col in pokemons_df.columns})
+        offset = next_offset
+        state["offset"] = offset
+        op.checkpoint(state)
+        api_endpoint = next_url
+        if next_url is None:
+            break
 
 
-# This creates the connector object that will use the update function defined in this connector.py file.
-connector = Connector(update=update, schema=schema)
+def get_data(url, offset):
+    """
+    This method fetches the data from the API
+    Args:
+        url: The URL of the API endpoint to fetch data from.
+        offset: The offset to be used for pagination, indicating how many records to skip.
+    Returns:
+        next_url: The URL for the next page of results, or None if there are no more pages.
+        pokemons_df: A DataFrame containing the results of the API call, with columns for "name" and "url".
+        offset: The updated offset value after fetching the current page of results.
+    """
+    response = rq.get(url)
+    data = response.json()
+    next_url = data["next"]
+    pokemons = data["results"]
+    pokemons_df = pd.DataFrame([])
+    for i in range(len(pokemons)):
+        pokemon_data = {"name": pokemons[i]["name"], "url": pokemons[i]["url"]}
+        pokemons_df = pd.concat([pokemons_df, pd.DataFrame([pokemon_data])], ignore_index=True)
+
+    return next_url, pokemons_df, offset + len(pokemons)
+
+
+# This creates the connector object that will use the update and schema functions defined in this connector.py file.
+connector = Connector(update=update)
 
 # Check if the script is being run as the main module.
 # This is Python's standard entry method allowing your script to be run directly from the command line or IDE 'run' button.
 # This is useful for debugging while you write your code. Note this method is not called by Fivetran when executing your connector in production.
-# Please test using the Fivetran debug command prior to finalizing and deploying your connector.
+# Please test using the "fivetran debug" command prior to finalizing and deploying your connector.
 if __name__ == "__main__":
-    # Adding this code to your `connector.py` allows you to test your connector by running your file directly from your IDE:
+    # Adding this code to your `connector.py` allows you to test your connector by running your file directly from your IDE.
     connector.debug()
-
-# Resulting table:
-# ┌───────────────────┬──────────────────────────────────┐
-# │      name         │             timestamp            │
-# │     varchar       │      timestamp with time zone    │
-# ├───────────────────┼──────────────────────────────────│
-# │      Event1       │  2024-09-24 14:30:45.000 +0000   │
-# │      Event2       │  2024-09-24 10:30:45.000 +0000   │
-# └───────────────────┴──────────────────────────────────┘
